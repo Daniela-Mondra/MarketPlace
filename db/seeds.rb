8d1e93f4d82eedec70b4bb5f7d9a133f019cc059
Jessica Manturano <jessica95.mp@gmail.com>
2024-09-03 11:45:14 -0500

    advance seed vinyl

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# validates :genre, inclusion: { in: %w[Rock Pop Electronic Jazz Hip-hop] } require 'faker'
+require 'faker'
+
+%w[Rock Pop Electronic Jazz Hip-hop].each do |genre_music|
+  3.times do
+    vinyl = Vinyl.new(
+      title: Faker::Music.album,
+      artist: Faker::Music.band,
+      genre: genre_music,
+      price: Faker::Number.between(from: 40, to: 250),
+      user_id: )
+    vinyl.save
+  end
+end
