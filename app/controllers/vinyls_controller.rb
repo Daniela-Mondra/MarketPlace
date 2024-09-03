b56e260e8df90fb1ee96dbb000f65fa2fbbbfa2e
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <diegoby1994@gmail.com>
2024-09-03 12:51:53 -0500

    edit and update vinyls

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,6 +1,21 @@
 class VinylsController < ApplicationController
-  
+
   def index
     @vinyls = Vinyl.all
+  end
+
+  def edit
+    @vinyl = Vinyl.find(params[:id])
+  end
+
+  def update
+    @vinyl = Vinyl.find(params[:id])
+    redirect_to vinyls_path, notice: 'Vinyl was successfully updated.' if @vinyl.update(vinyl_params)
+  end
+
+  private
+
+  def vinyl_params
+    params.require(:vinyl).permit(:title, :artist, :genre, :price)
   end
 end
