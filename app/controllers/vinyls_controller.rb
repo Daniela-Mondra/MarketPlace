1fb82ecb37ed7eaad111c5a73353898c0a4ce374
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <diegoby1994@gmail.com>
2024-09-03 13:03:12 -0500

    destroy vinyls

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,6 +1,11 @@
 class VinylsController < ApplicationController
-  
+
   def index
     @vinyls = Vinyl.all
+  end
+
+  def destroy
+    @vinyl.destroy
+    redirect_to vinyls_path, notice: 'Deleted'
   end
 end
