e4462397c22abd96a44d99ba0679677349d3a709
hugo salazar <hugog.salazarl@gmail.com>
2024-09-03 14:15:03 -0500

    vinyl-controller-show-view

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,6 +1,15 @@
 class VinylsController < ApplicationController
-  
+  before_action :set_vinyls, only: :show
   def index
     @vinyls = Vinyl.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_vinyls
+    @vinyl = Vinyl.find(params[:id])
   end
 end
