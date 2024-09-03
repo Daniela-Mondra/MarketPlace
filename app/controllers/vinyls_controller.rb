0bc86a40acb20f00bfeea6bd9f40a4ebc6d22731
Daniela Mondragon <dani.mariana2901@gmail.com>
2024-09-03 12:05:15 -0600

    Se añadieron los métodos new y create

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,6 +1,27 @@
 class VinylsController < ApplicationController
-  
+
   def index
     @vinyls = Vinyl.all
+  end
+
+  def new
+    @vinyl = Vinyl.new
+  end
+
+  def create
+    @vinyl = Vinyl.new(vinyl_params)
+    @vinyl.user = current_user
+
+    if @vinyl.save
+      redirect_to vinyls_path(@vinyl), notice: 'Vinyl was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def vinyl_params
+    params.require(:vinyl).permit(:title, :artist, :genre, :price)
   end
 end
