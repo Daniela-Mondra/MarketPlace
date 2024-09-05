cc77f4430af640c449ea5f17af0058966e6f28b9
Jessica Manturano <jessica95.mp@gmail.com>
2024-09-05 15:34:04 -0500

    advance carousel

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,8 +1,67 @@
+// import { Controller } from "@hotwired/stimulus"
+
+// export default class extends Controller {
+//   static targets = ["item", "container"]
+
+//   connect() {
+//     this.initializeCarousel()
+//   }
+
+//   initializeCarousel() {
+//     const items = this.itemTargets
+//     const minPerSlide = 3
+
+//     items.forEach((el) => {
+//       let next = el.nextElementSibling
+//       for (let i = 1; i < minPerSlide; i++) {
+//         if (!next) {
+//           next = items[0]
+//         }
+//         let cloneChild = next.cloneNode(true)
+//         el.appendChild(cloneChild.children[0])
+//         next = next.nextElementSibling
+//       }
+//     })
+//   }
+
+//   previous() {
+//     this.containerTarget.carousel('prev')
+//   }
+
+//   next() {
+//     this.containerTarget.carousel('next')
+//   }
+// }
+// app/javascript/controllers/carousel_controller.js
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="carousel"
 export default class extends Controller {
+  static targets = ["container"]
+
   connect() {
-    console.log("hola 3");
+    this.carouselElement = this.containerTarget
+    this.carouselWidth = this.carouselElement.querySelector(".carousel-inner").scrollWidth
+    this.cardWidth = this.carouselElement.querySelector(".carousel-item").offsetWidth
+    this.scrollPosition = 0
+  }
+
+  scrollNext() {
+    if (this.scrollPosition < this.carouselWidth - this.cardWidth * 4) {
+      this.scrollPosition += this.cardWidth
+      this.carouselElement.querySelector(".carousel-inner").scroll({
+        left: this.scrollPosition,
+        behavior: "smooth"
+      })
+    }
+  }
+
+  scrollPrev() {
+    if (this.scrollPosition > 0) {
+      this.scrollPosition -= this.cardWidth
+      this.carouselElement.querySelector(".carousel-inner").scroll({
+        left: this.scrollPosition,
+        behavior: "smooth"
+      })
+    }
   }
 }
