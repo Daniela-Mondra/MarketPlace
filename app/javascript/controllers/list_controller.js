e718ab94cc6c511dbb5b4c96e5c4f5a47b7790d7
Jessica Manturano <jessica95.mp@gmail.com>
2024-09-05 23:00:00 -0500

    add mapbox address login

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["container"]
   connect() {
-    console.log("conectado")
     this.containerTarget.scrollIntoView({behavior: "smooth"});
   }
 
